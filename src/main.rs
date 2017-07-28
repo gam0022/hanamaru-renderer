34268aa9079109c6ed9bde00c623a5b361a53680
gam0022 <gam0022@gmail.com>
2017-07-29 01:48:49 +0900

    Run fractal example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+extern crate num;
+extern crate image;
+
+use std::fs::File;
+use std::path::Path;
+
+use num::complex::Complex;
+
 fn main() {
-    println!("Hello, world!");
+    let max_iterations = 256u16;
+
+    let imgx = 800;
+    let imgy = 800;
+
+    let scalex = 4.0 / imgx as f32;
+    let scaley = 4.0 / imgy as f32;
+
+    // Create a new ImgBuf with width: imgx and height: imgy
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let cy = y as f32 * scaley - 2.0;
+        let cx = x as f32 * scalex - 2.0;
+
+        let mut z = Complex::new(cx, cy);
+        let c = Complex::new(-0.4, 0.6);
+
+        let mut i = 0;
+
+        for t in 0..max_iterations {
+            if z.norm() > 2.0 {
+                break
+            }
+            z = z * z + c;
+            i = t;
+        }
+
+        // Create an 8bit pixel of type Luma and value i
+        // and assign in to the pixel at position (x, y)
+        *pixel = image::Luma([i as u8]);
+
+    }
+
+
+    // Save the image as “fractal.png”
+    let ref mut fout = File::create(&Path::new("fractal.png")).unwrap();
+
+    // We must indicate the image’s color type and what format to save as
+    let _ = image::ImageLuma8(imgbuf).save(fout, image::PNG);
 }
