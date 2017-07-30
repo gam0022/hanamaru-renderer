758eee6c77e9d44fb10355f1da38a29faa4952ee
gam0022 <gam0022@gmail.com>
2017-07-30 20:36:46 +0900

    Move writing image to DebugRenderer

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod renderer;
 
 use vector::{Vector3, Vector2};
 use scene::{Scene, Camera, Ray, Intersectable, Sphere, Intersection};
-use renderer::DebugRenderer;
+use renderer::{Renderer, DebugRenderer};
 
 fn main_test() {
     let width = 800;
@@ -19,6 +19,7 @@ fn main_test() {
     scene::test();
 
     let mut imgbuf = image::ImageBuffer::new(width, height);
+
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let u = x as f64 / width as f64;
@@ -33,34 +34,24 @@ fn main_test() {
 fn main() {
     let width = 800;
     let height = 600;
-    let resolution = Vector2::new(width as f64, height as f64);
-
     let mut imgbuf = image::ImageBuffer::new(width, height);
 
     let camera = Camera::new(
-        Vector3::new(0.0, 0.0, -3.0),
+        Vector3::new(0.0, 0.0, 3.0),
         Vector3::new(0.0, 0.0, 0.0),
         Vector3::new(0.0, 1.0, 0.0),
         1.5
     );
-    println!("{:?}", camera);
 
     let scene = Scene {
         elements: vec![
             Box::new(Sphere{ center: Vector3::new(0.0, 0.0, 0.0), radius: 1.0 }),
+            Box::new(Sphere{ center: Vector3::new(-2.0, 0.0, -1.0), radius: 0.5 }),
         ],
     };
 
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let frag_coord = Vector2::new(x as f64, y as f64);
-        let uv = (frag_coord * 2.0 - resolution) / resolution.x.min(resolution.y);
-        let color = DebugRenderer::test(&scene, &camera, &uv);
-        *pixel = image::Rgb([
-            (255.0 * color.x) as u8,
-            (255.0 * color.y) as u8,
-            (255.0 * color.z) as u8,
-        ]);
-    }
+    let renderer = DebugRenderer{};
+    renderer.render(&scene, &camera, &mut imgbuf);
 
     let ref mut fout = File::create(&Path::new("test.png")).unwrap();
     let _ = image::ImageRgb8(imgbuf).save(fout, image::PNG);
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,17 +1,18 @@
+extern crate image;
+use image::{ImageBuffer, Pixel, Rgb};
+
 use consts;
 use vector::{Vector3, Vector2};
 use scene::{Scene, Camera, Ray, Intersectable, Sphere, Intersection};
 
 pub trait Renderer {
-    //fn calc_radiance(ray: &mut Ray) -> Vector3;
+    fn calc_pixel(&self, scene: &Scene, camera: &Camera, uv: &Vector2) -> Vector3;
 }
 
-pub struct DebugRenderer {
-}
+pub struct DebugRenderer;
 
 impl DebugRenderer {
-    //fn calc_radiance(ray: &mut Ray) -> Vector3 {
-    pub fn test(scene: &Scene, camera: &Camera, uv: &Vector2) -> Vector3 {
+    pub fn calc_pixel(&self, scene: &Scene, camera: &Camera, uv: &Vector2) -> Vector3 {
        let ray = camera.shoot_ray(&uv);
        let intersection = scene.intersect(&ray);
 
@@ -19,6 +20,20 @@ impl DebugRenderer {
            Vector3::new(0.0, 0.0, 0.0)
        } else {
            Vector3::new(1.0, 1.0, 1.0)
+       }
+   }
+
+   pub fn render(&self, scene: &Scene, camera: &Camera, imgbuf: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) {
+       let resolution = Vector2::new(imgbuf.width() as f64, imgbuf.height() as f64);
+       for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+           let frag_coord = Vector2::new(x as f64, y as f64);
+           let uv = (frag_coord * 2.0 - resolution) / resolution.x.min(resolution.y);
+           let color = self.calc_pixel(&scene, &camera, &uv);
+           *pixel = image::Rgb([
+               (255.0 * color.x) as u8,
+               (255.0 * color.y) as u8,
+               (255.0 * color.z) as u8,
+           ]);
        }
    }
 }
