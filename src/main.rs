c320484e67c9c468a9d3bfa5a2eb563900a526f2
gam0022 <gam0022@gmail.com>
2017-08-05 17:23:52 +0900

    add timer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate num;
 extern crate image;
+extern crate time;
 use std::fs::File;
 use std::path::Path;
 
@@ -16,24 +17,7 @@ use scene::{Scene, CameraBuilder, Sphere, Plane};
 use material::{Material, SurfaceType};
 use renderer::{Renderer, DebugRenderer, PathTracingRenderer};
 
-#[allow(dead_code)]
-fn main_gradation() {
-    let width = 800;
-    let height = 600;
-
-    let mut imgbuf = image::ImageBuffer::new(width, height);
-
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let u = x as f64 / width as f64;
-        let v = y as f64 / height as f64;
-        *pixel = image::Rgb([(255.0 * u) as u8, (255.0 * v) as u8, 127]);
-    }
-
-    let ref mut fout = File::create(&Path::new("test.png")).unwrap();
-    let _ = image::ImageRgb8(imgbuf).save(fout, image::PNG);
-}
-
-fn main() {
+fn render() {
     let width = 800;
     let height = 600;
     let mut imgbuf = image::ImageBuffer::new(width, height);
@@ -76,4 +60,11 @@ fn main() {
 
     let ref mut fout = File::create(&Path::new("test.png")).unwrap();
     let _ = image::ImageRgb8(imgbuf).save(fout, image::PNG);
+}
+
+fn main() {
+    let begin = time::now();
+    render();
+    let end = time::now();
+    println!("total {} sec.", (end - begin).num_milliseconds() as f64 * 0.001);
 }
