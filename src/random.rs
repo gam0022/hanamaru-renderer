84114bb7d5b3f6fdbf97f7bebb562fa88d4d7910
gam0022 <gam0022@gmail.com>
2017-08-15 18:27:05 +0900

    Refactor random

--- src/random.rs
+++ src/random.rs
@@ -3,5 +3,5 @@ extern crate rand;
 use self::rand::{Rng, ThreadRng};
 
 pub fn get_random(rng: &mut ThreadRng) -> (f64, f64) {
-    (rng.gen_range(0.0, 1.0), rng.gen_range(0.0, 1.0))
+    rng.gen::<(f64, f64)>()
 }
