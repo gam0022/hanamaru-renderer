aacbc0c08e6092ef5df410f4ed3dfa7a776e8b9e
gam0022 <gam0022@gmail.com>
2018-09-02 10:37:37 +0900

    Use Simple Scene

--- src/config.rs
+++ src/config.rs
@@ -14,12 +14,12 @@ pub const SUPERSAMPLING: u32 = 2;
 pub const PATHTRACING_BOUNCE_LIMIT: u32 = 10;
 
 // Tone Mapping
-pub const TONE_MAPPING_MODE: ToneMappingMode = ToneMappingMode::Reinhard;
+pub const TONE_MAPPING_MODE: ToneMappingMode = ToneMappingMode::None;
 pub const TONE_MAPPING_EXPOSURE: f64 = 1.5;
 pub const TONE_MAPPING_WHITE_POINT: f64 = 20.0;
 
 // Denoising - Bilateral Fileter
-pub const BILATERAL_FILTER_ITERATION: u32 = 1;
+pub const BILATERAL_FILTER_ITERATION: u32 = 0;
 pub const BILATERAL_FILTER_DIAMETER: u32 = 3;
 pub const BILATERAL_FILTER_SIGMA_I: f64 = 1.0;// これを無限大にすると Gaussian Blur となる
 pub const BILATERAL_FILTER_SIGMA_S: f64 = 16.0;
